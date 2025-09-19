445b8cb1d2199fe78daee7f7002ffdfa0da9fffa
contact372 <contact@maigic.pro>
2025-09-19 16:48:08 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,36 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '4.5mb', // Default is 1mb, combined images can exceed this.
-    },
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      }
-    ],
-  },
-  // Make the environment variable available to the client-side code
-  env: {
-    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+  output: 'standalone',
+  webpack: (config, {
+    isServer
+  }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        "firebase-admin": false,
+      };
+    }
+    return config;
   },
 };
 
