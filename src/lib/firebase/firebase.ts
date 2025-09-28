d2bee1ac8750a79209162dffa6344b5390b31d96
contact372 <contact@maigic.pro>
2025-09-28 16:32:42 +0200

    Update firebase.ts

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -1,21 +1,15 @@
-
-import { getApp, getApps, initializeApp } from 'firebase/app';
-import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
-import { getAuth, connectAuthEmulator } from 'firebase/auth';
-import { getFunctions, connectFunctionsEmulator } from 'firebase/functions';
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getFunctions } from 'firebase/functions';
 import { firebaseConfig } from './config';
 
+// Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const db = getFirestore(app);
 const auth = getAuth(app);
-const functions = getFunctions(app);
+const db = getFirestore(app);
 
-// Connect to emulators in development
-if (process.env.NODE_ENV === 'development') {
-    // Point to the emulators running on localhost.
-    connectFirestoreEmulator(db, 'localhost', 8080);
-    connectAuthEmulator(auth, 'http://localhost:9099');
-    connectFunctionsEmulator(functions, 'localhost', 5001);
-}
+// Specify the region for the Cloud Functions
+const functions = getFunctions(app, 'europe-west1');
 
-export { app, db, auth, functions };
+export { app, auth, db, functions };
