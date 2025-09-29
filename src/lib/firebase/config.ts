6af0477fce017090b11f2b821d5c5ed5627e1451
contact372 <contact@maigic.pro>
2025-09-29 16:42:36 +0200

    Update config.ts

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,4 +1,4 @@
 // This file is intentionally left blank.
 // The Firebase configuration will be populated by the App Prototyper.
 // IMPORTANT: DO NOT MODIFY THIS FILE MANUALLY.
-export const firebaseConfig = {"projectId":"eternal-kiss","appId":"1:316089508416:web:c9821e03540a8eb7dbb5a5","storageBucket":"eternal-kiss.firebasestorage.app","apiKey":"AIzaSyD8ZwCINnnXjUQ6aFj654n8jVEsIB-oJNY","authDomain":"eternal-kiss.firebaseapp.com","measurementId":"","messagingSenderId":"316089508416"};
+export const firebaseConfig = {"apiKey":"AIzaSyDLqLCo2zi-qLNYyNe1AwehXSfjtUGGIcc","authDomain":"gen-lang-client-0395827708.firebaseapp.com","projectId":"gen-lang-client-0395827708","storageBucket":"gen-lang-client-0395827708.firebasestorage.app","messagingSenderId":"85782219182","appId":"1:85782219182:web:894cbd7ac7f95d393d203f"};
