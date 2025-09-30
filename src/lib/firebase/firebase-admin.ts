6c70424d555b97e0f7faa65f35f36aeb94119f32
contact372 <contact@maigic.pro>
2025-10-01 01:29:30 +0200

    Update firebase-admin.ts

--- src/lib/firebase/firebase-admin.ts
+++ src/lib/firebase/firebase-admin.ts
@@ -1,21 +1,13 @@
 
-import admin from "firebase-admin";
+import * as admin from 'firebase-admin';
 
-// Check if the app is already initialized to prevent errors in serverless environments
+// When running on Google Cloud, the SDK automatically discovers the project configuration.
 if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      // Use `\n` for newlines in environment variables
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    }),
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  });
+  admin.initializeApp();
 }
 
 const db = admin.firestore();
 const auth = admin.auth();
+const storage = admin.storage();
 
-export { db, auth };
-
+export { db, auth, storage };
